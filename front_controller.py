0358eb24899a4e1b979bffd1db22d3695094ab33
seada1 <105821673+seada1@users.noreply.github.com>
2022-05-19 16:19:55 -0500

    Delete front_controller.py

--- front_controller.py
+++
@@ -1,9 +0,0 @@
-# Front Controller design pattern - use a single controller as a point of entry into Our application.
-# The front controller will then designate the flow of control to the appropriate controller.
-from controllers import movie_controller, home_controller
-
-
-def route(app):
-    # Call all other controllers
-    home_controller.route(app)
-    movie_controller.route(app)
