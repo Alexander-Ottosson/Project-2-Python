9ba6a281dc0b0c8da10a55778517984265da71b6
aottosson <cMoShBd3>
2022-05-20 10:04:52 -0500

    Renamed instances of "id" to "m_id". Added app.route to the delete_mech method.

--- controllers/mech_controller.py
+++ controllers/mech_controller.py
@@ -1,22 +1,24 @@
 from flask import request, jsonify
 from exceptions.resource_not_found import ResourceNotFound
-from models.Mech import Mech
-from repositories.mech_repo_impl import MechRepoImpl
+from Class.Mech import Mech
+from Repo.Mech_repo import Mechrepo
+
+# TODO:  create MechService class
 from services.mech_service import MechService
 
-mr = MechRepoImpl()
+mr = Mechrepo()
 ms = MechService(mr)
 
 
 def route(app):
     @app.route("/mech", methods=['GET'])
-    def get_all_Mechs():
-        return jsonify([Mech.json() for Mech in ms.get_all_Mechs()])
+    def get_all_mechs():
+        return jsonify([mech.json() for mech in ms.get_all_Mechs()])
 
     @app.route("/mech/<id>", methods=['GET'])
-    def get_mech(id):
+    def get_mech(m_id):
         try:
-            return ms.get_mech_by_id(int(id)).json(), 200
+            return ms.get_mech_by_id(int(m_id)).json(), 200
         except ValueError as e:
             return "Not a valid ID", 400  # Bad Request
         except ResourceNotFound as r:
@@ -26,19 +28,21 @@ def route(app):
     def post_mech():
         body = request.json
 
-        mech = Mech(make=body["make"], model=body["model"], year=body["year"], color=body["color"], max_speed=body["max_speed"], weight=body["weight"], height=body["height"], description=body["description"], current_pilot=body["current_pilot"],pilot_count=body["pilot_count"],available=body["available"], confidential=body["confidential"])
+        mech = Mech(
+            make=body["make"], model=body["model"], year=body["year"], color=body["color"], max_speed=body["max_speed"], weight=body["weight"], height=body["height"], description=body["description"], current_pilot=body["current_pilot"],pilot_count=body["pilot_count"],available=body["available"], confidential=body["confidential"])
         mech = ms.create_mech(mech)
 
         return mech.json()
 
     @app.route("/mech/<id>", methods=["PUT"])
-    def put_movie(id):
+    def put_movie(m_id):
         body = request.json
-        mech = Mech(id=body["id"], make=body["make"], model=body["model"], year=body["year"], color=body["color"], max_speed=body["max_speed"], weight=body["weight"], height=body["height"], description=body["description"], current_pilot=body["current_pilot"],pilot_count=body["pilot_count"],available=body["available"], confidential=body["confidential"])
+        mech = Mech(m_id=m_id, make=body["make"], model=body["model"], year=body["year"], color=body["color"], max_speed=body["max_speed"], weight=body["weight"], height=body["height"], description=body["description"], current_pilot=body["current_pilot"],pilot_count=body["pilot_count"],available=body["available"], confidential=body["confidential"])
         mech = ms.update_mech(mech)
 
         return mech.json()
 
-    def delete_mech(id):
-        ms.delete_mech(id)
+    @app.route("/mech/<m_id>", methods=['DELETE'])
+    def delete_mech(m_id):
+        ms.delete_mech(m_id)
         return '', 204  # No Content
