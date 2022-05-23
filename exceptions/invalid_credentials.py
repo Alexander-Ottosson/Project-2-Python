6d7a10c0ba78b7c103879962d2b72f4d50665616
dcantoran <41706170+dcantoran@users.noreply.github.com>
2022-05-23 10:54:10 -0400

    Delete exceptions directory

--- exceptions/invalid_credentials.py
+++
@@ -1,4 +0,0 @@
-class InvalidCredentials(Exception):
-    def __init__(self, message):
-        self.message = message
-        
--- exceptions/mech_unavailable.py
+++
@@ -1,4 +0,0 @@
-# This exception should be thrown if a pilot tries to check out an unavailable mech
-class MechUnavailable(Exception):
-    def __init__(self, message):
-        self.message = message
