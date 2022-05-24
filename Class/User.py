d43348a13f6b71d1b38a61d21bfc9232ef3c26dd
David Cantoran <davidcantoran36@gmail.com>
2022-05-24 11:22:01 -0400

    updated the class, repo, and service for users with firstname and lastname

--- Class/User.py
+++ Class/User.py
@@ -1,18 +1,21 @@
 class User:
 
-    def __init__(self,u_id=0, username="", password="", is_pilot=False, is_admin=False):
+    def __init__(self, u_id=0, username="", password="", first_name="", last_name="", is_pilot=False, is_admin=False):
         self.u_id = u_id
         self.username = username
         self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
         self.is_pilot = is_pilot
         self.is_admin = is_admin
-
 
     def __repr__(self):
         return str({
             'id': self.u_id,
             'username': self.username,
-            "Password": self.password,
+            "password": self.password,
+            "firstname": self.first_name,
+            "lastname": self.last_name,
             'is_pilot': self.is_pilot,
             'is_admin': self.is_admin
         })
@@ -22,6 +25,8 @@ class User:
             'Id': self.u_id,
             'Username': self.username,
             "Password": self.password,
+            "firstname": self.first_name,
+            "lastname": self.last_name,
             'IsPilot': self.is_pilot,
             'IsAdmin': self.is_admin
         }
